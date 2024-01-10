771da7d7cba0c5670acd84bea7901cbb44f8e6a5
ANUPAM-RO <anupamroy575@gmail.com>
2024-01-10 11:34:55 +0530

    add switch button

--- components/MainBody.js
+++ components/MainBody.js
@@ -107,6 +107,26 @@ const MainBody = () => {
       <div className="text-2xl font-bold text-red-500 flex justify-center pb-4 pt-4">
         Projects Minamata
       </div>
+      <div className="flex flex-col items-center">
+        <div>
+          <label className="relative inline-flex items-center mb-5 cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" disabled />
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ms-3 text-lg font-medium text-gray-400 dark:text-gray-500">
+              Employee
+            </span>
+          </label>
+        </div>
+        <div className="mr-5">
+          <label className="relative inline-flex items-center mb-5 cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" disabled />
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ms-3 text-lg font-medium text-gray-400 dark:text-gray-500">
+              Owner
+            </span>
+          </label>
+        </div>
+      </div>
       <div>
         <p className="text-xl font-bold text-red-700 p-4">Previous Projects:</p>
         <div className=" grid grid-cols-3 md:grid-cols-2 gap-4 ">
